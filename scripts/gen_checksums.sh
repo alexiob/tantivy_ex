2caf775f81709043f7a647462d0defc36a4fcb8b
Alessandro Iob <alessandro.iob@gmail.com>
2025-07-30 14:47:30 +0200

    fix: cleanup script

--- scripts/gen_checksums.sh
+++ scripts/gen_checksums.sh
@@ -11,23 +11,3 @@ VERSION="0.4.1"
 assets=$(gh release view v$VERSION --json assets --jq '.assets[] | select(.name | test("\\.tar\\.gz$")) | .browser_download_url + " \"" + .name + "\" => \"" + .digest + "\","')
 
 (echo "%{$assets}")  > "$OUT_FILE"
-# declare -A checksums
-
-# # Download each asset and compute its sha256
-# while read -r url name; do
-#   echo "Processing $name"
-#   curl -sL "$url" -o "$TMP_DIR/$name"
-#   sha=$(shasum -a 256 "$TMP_DIR/$name" | awk '{print $1}')
-#   checksums["$name"]="sha256:$sha"
-# done <<< "$assets"
-
-# # Generate the Elixir map
-# {
-#   echo "%{"
-#   for name in "${!checksums[@]}"; do
-#     echo "  \"$name\" => \"${checksums[$name]}\","
-#   done
-#   echo "}"
-# } > "$OUT_FILE"
-
-# echo "Checksums written to $OUT_FILE"
